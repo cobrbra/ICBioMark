56fe680e107d98679457f20fde4a9198c4d488af
Jacob Bradley <cobrbradley@gmail.com>
2020-12-30 17:00:31 +0000

    Fixed bad markdown in nsclc_survival docs

--- R/nsclc_data.R
+++ R/nsclc_data.R
@@ -21,29 +21,29 @@
 #' corresponds to a sample, and details clinical and survival information about the
 #' patient from whom the sample was derived. Important columns :
 #'   \describe{
-#'     \item{CASE_ID}
-#'     \item{AGE}
-#'     \item{AGE_AT_SURGERY}
-#'     \item{CANCER_TYPE}
-#'     \item{CANCER_TYPE_DETAILED}
-#'     \item{DAYS_TO_DEATH}
-#'     \item{DAYS_TO_LAST_FOLLOWUP}
-#'     \item{FRACTION_GENOME_ALTERED}
-#'     \item{HISTORY_NEOADJUVANT_TRTYN}
-#'     \item{HISTORY_OTHER_MALIGNANCY}
-#'     \item{MUTATION_COUNT}
-#'     \item{M_STAGE}
-#'     \item{N_STAGE}
-#'     \item{ONCOTREE_CODE}
-#'     \item{OS_MONTHS}
-#'     \item{OS_STATUS}
-#'     \item{SAMPLE_COUNT}
-#'     \item{SEX}
-#'     \item{SMOKING_HISTORY}
-#'     \item{SMOKING_PACK_YEARS}
-#'     \item{SOMATIC_STATUS}
-#'     \item{STAGE}
-#'     \item{T_STAGE}
+#'     \item{CASE_ID}{}
+#'     \item{AGE}{}
+#'     \item{AGE_AT_SURGERY}{}
+#'     \item{CANCER_TYPE}{}
+#'     \item{CANCER_TYPE_DETAILED}{}
+#'     \item{DAYS_TO_DEATH}{}
+#'     \item{DAYS_TO_LAST_FOLLOWUP}{}
+#'     \item{FRACTION_GENOME_ALTERED}{}
+#'     \item{HISTORY_NEOADJUVANT_TRTYN}{}
+#'     \item{HISTORY_OTHER_MALIGNANCY}{}
+#'     \item{MUTATION_COUNT}{}
+#'     \item{M_STAGE}{}
+#'     \item{N_STAGE}{}
+#'     \item{ONCOTREE_CODE}{}
+#'     \item{OS_MONTHS}{}
+#'     \item{OS_STATUS}{}
+#'     \item{SAMPLE_COUNT}{}
+#'     \item{SEX}{}
+#'     \item{SMOKING_HISTORY}{}
+#'     \item{SMOKING_PACK_YEARS}{}
+#'     \item{SOMATIC_STATUS}{}
+#'     \item{STAGE}{}
+#'     \item{T_STAGE}{}
 #'    }
 #' @source \url{https://www.cbioportal.org/study/clinicalData?id=nsclc_tcga_broad_2016}
 #'
