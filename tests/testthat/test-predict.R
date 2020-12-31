393c0f411a61543ca00b2cf040c68d450713944b
Jacob Bradley <cobrbradley@gmail.com>
2020-12-31 00:53:18 +0000

    equal_to_reference test for pred_first_fit

---
+++ tests/testthat/test-predict.R
@@ -0,0 +1,8 @@
+context("Functions for fitting predictive models")
+
+test_that("pred_first_fit gives reference value", {
+  output <- pred_first_fit(example_gen_model, lambda = exp(seq(-9, -14, length.out = 100)),
+                           training_matrix = example_tables$train$matrix,
+                           gene_lengths = example_maf_data$gene_lengths)
+  expect_equal(output, example_pred_fit)
+})
